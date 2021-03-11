7a9a7197080e39909aa30d551f5c47bc3083a8b6
nickbevers <nick.bevers9@gmail.com>
2021-03-11 14:34:39 +0100

    Added the fetch data for corona cases; Added the if-else statement in the setAd() function to show party poppers if #cases today is < 3000

--- lab4/script.js
+++ lab4/script.js
@@ -1,19 +1,49 @@
+//used API's:
+// https://api.bol.com/mondkapjesstore/inventory
+// https://coronavirus-19-api.herokuapp.com/countries/Belgium
+
 class App{
     constructor(){
         this.getData();
-        this.setAd();
     }
 
     getData(){
-        fetch("https://api.exchangeratesapi.io/latest?")
+        fetch("https://api.exchangeratesapi.io/latest?symbols=USD,GBP,JPY")
         .then(response => response.json())
-        .then( data => console.log(data) )
+        .then( data => {
+            console.log(data);
+            let pound = data.rates.GBP;
+            console.log(pound);
+            let dollar = data.rates.USD;
+            console.log(dollar);
+            let yen = data.rates.JPY;
+            console.log(yen);
+        })
         .catch(err => console.error(err));
+
+        
+        fetch("https://coronavirus-19-api.herokuapp.com/countries/Belgium")
+        .then(response => response.json())
+        .then (data => {
+            let cases = data.todayCases;
+            console.log(cases);
+            this.setAd(cases);
+        });
+
+
     }
 
-    setAd(){
-        // https://api.bol.com/mondkapjesstore/inventory
-        // https://coronavirus-19-api.herokuapp.com/countries/Belgium
+    setAd(cases){
+        if (cases > 3000){
+            document.querySelector(".ad").style.backgroundImage = "url(https://media.s-bol.com/RLLj3q3W9LGO/550x412.jpg)";
+            document.querySelector(".ad").style.backgroundSize = "contain";
+            document.querySelector(".ad").style.backgroundRepeat = "no-repeat";
+        }
+        else{
+            document.querySelector(".ad").style.backgroundImage = "url(https://media.s-bol.com/g2PG1K4qqEL6/497x840.jpg)";
+            document.querySelector(".ad").style.backgroundSize = "contain";
+            document.querySelector(".ad").style.backgroundRepeat = "no-repeat";
+        }
     }
 }
 let app = new App();
