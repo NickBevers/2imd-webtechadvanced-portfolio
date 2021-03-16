9857a9fafb8927dfa0cc3fc6decb94b377775a69
nickbevers <nick.bevers9@gmail.com>
2021-03-16 10:15:03 +0100

    Added bol.com logo in assets folder; Added description and buy now button in html; Added styling to the button; href of button changes depending on #corona cases today

--- lab4/script.js
+++ lab4/script.js
@@ -46,23 +46,27 @@ class App{
             document.querySelector(".ad").style.backgroundImage = "url(https://media.s-bol.com/RLLj3q3W9LGO/550x412.jpg)";
             document.querySelector(".ad").style.backgroundSize = "contain";
             document.querySelector(".ad").style.backgroundRepeat = "no-repeat";
+            document.querySelector("#buyLink").setAttribute("href", "https://bit.ly/2OYMHTO");
+            document.querySelector(".desc").innerHTML = "Er zijn weer heel wat corona-gevallen bijgekomen in België. Bescherm jezelf nu!"
         }
         else{
             document.querySelector(".ad").style.backgroundImage = "url(https://media.s-bol.com/g2PG1K4qqEL6/497x840.jpg)";
             document.querySelector(".ad").style.marginLeft = "120px";
             document.querySelector(".ad").style.backgroundSize = "contain";
             document.querySelector(".ad").style.backgroundRepeat = "no-repeat";
+            document.querySelector("#buyLink").setAttribute("href", "http://bit.ly/3cAvMPl");
+            document.querySelector(".desc").innerHTML = "Hoera, er is een daling in de corona-cijfers in België. Vier het nu!"
         }
     }
 
     setPrice(cases, price, pound, dollar, yen){
         // console.log(cases);
         if (cases > 3000){
-            document.querySelector(".title").innerHTML = `Bescherm jezelf en koop nu mondmaskers voor maar € ${price}`;
+            document.querySelector(".title").innerHTML = `Koop nu 100 mondmaskers voor maar € ${price} bij Bol.com` ;
         }
         else{
             // console.log(cases);
-            document.querySelector(".title").innerHTML = `Koop nu 3 partypoppers voor maar € ${price}`;
+            document.querySelector(".title").innerHTML = `Koop nu 3 partypoppers voor maar € ${price} bij Bol.com`;
         }
         
         document.querySelector(".euro").innerHTML = `€ ${price}`;
