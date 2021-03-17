431cee673244bc4ba3a116b55ccfb3a041864d4b
nickbevers <nick.bevers9@gmail.com>
2021-03-17 20:16:18 +0100

    Added all controllers

--- Lab5/index.js
+++ Lab5/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const routerMessages = require("./routes/index.js")
+const routerMessages = require("./routes/index")
 
 app.get("/", (req, res) => {
   app.set("view engine", "pug");
--- Lab5/routes/index.js
+++ Lab5/routes/index.js
@@ -1,64 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const messageController = require("../controllers/index");
 
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "GETTING messages"
-  })
-  //Geef messages terug (met mongoDB)
-  //(zonder mongoDb) Response kan zijn : { “message”: “GETTING messages” }
-  //geef ook effectief messages terug uit je databank (of statisch)
-}) 
 
-router.get("/:id", (req, res) => {
-  let id = req.params.id;
-  res.json({
-    message: `GETTING message with ID ${id}`
-  })
-  //(met mongoDb) Geeft message met specifiek id terug
-  //(zonder mongoDb) Response kan zijn : { “message”: “GETTING message with ID id” }
+router.get("/", messageController.getMessage) 
 
-}) 
+router.get("/:id", messageController.getId)
 
-router.post("/:user", (req, res) => {
-  let user = req.params.user;
-  res.json({
-    message: `POSTING a new message for user ${user}`
-  })
-  //(met mongoDb) Kan een JSON-object ontvangen en bewaren en geeft het nieuwe document terug
-  //Body: { message: { “user”: “Pikachu”, “text”: “nodejs isn’t hard, or is it?” } }
-  //(zonder mongoDb) Response kan zijn: {“message”: “POSTING a new message for user Pikachu”}
-  //Zorg dat je de naam dynamisch kan invullen en in het antwoord teruggeeft
-}) 
+router.post("/:user", messageController.setMessage)
 
-router.put("/:id", (req, res) => {
-  let id = req.params.id;
-  res.json({
-    message: `UPDATING a message with id ${id}`
-  })
-  //(met mongoDb) Kan een JSON-object ontvangen en een specifiek bericht updaten en geeft die nieuwe bericht terug
-  //(zonder MongoDb) Response kan zijn: {“message”: “UPDATING a message with id id”}
-})
+router.put("/:id", messageController.putId)
 
-router.delete("/:id", (req, res) => {
-  let id = req.params.id;
-  res.json({
-    message: `DELETING a message with id ${id}`
-  })
-  //(met mongoDb) Kan een message met id verwijderen en geeft een response terug {“status”: “success”, “message”: “The message was removed”}
-  //(zonder mongoDb) Response kan zij: {“message”: “DELETING a message with id id”}
-}) 
+router.delete("/:id", messageController.deleteId)
 
-router.get("/?user=username", (req, res) => {
-  let user = req.query.user;
-  res.json({
-    message: `GETTING a new message for user ${user}`
-  })
-  //(met mongoDb) Kan berichten teruggeven voor een bepaalde username
-  //Tip: req.params.username
-  //(zonder mongoDb) Response kan zijn: {“message”: “GETTING message for username username”}
-})
+router.get("/?user=username", messageController.getUser)
 
 
 module.exports = router;
